cbd5a153f81441e889a08e8037b7660a35f7298e
Blu3Hawks <ylb.hen.sayag@gmail.com>
2025-12-03 01:55:58 +0200

    Uploaded Project + 2 tasks
    The project was uploaded. In addition there are two more tasks - one of which is the movement input via pressing, and other is the start of the gyro writing. There needs to be done some polishing in the UI and other elements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,54 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using TMPro; 
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb;
+    [Header("Movement Settings")]
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float speed = 0;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody rb;
+    public TextMeshProUGUI countText; // ref to UI text component
+    public GameObject winTextObject;
+
+    //private variables
     private float movementX;
     private float movementY;
-    public float speed = 0;
     private int count;
-    public TextMeshProUGUI countText; // ref to UI text component
-    public GameObject winTextObject; 
 
     // Start is called before the first frame update
     // check every frame for player input
     // apply that to player game object as movement
     // fixed update <= called just before physics calculations
-    void Start()
+    private void Start()
     {
+        Input.gyro.enabled = true;//for our bonus - make the gyro enabled
         count = 0; //sets initial count to zero
-        rb = GetComponent<Rigidbody>();
-
-        setCountText();
+        SetCountText();
 
         winTextObject.SetActive(false);
     }
 
-    void OnMove(InputValue movementVal)
+    private void FixedUpdate()
     {
-        //getting input value from player
-        // gets vector 2 data from movement value and stores it into the vector2 variable = movementVector
-        Vector2 movementVector = movementVal.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        MouseMovement();
+        GyroscopeMovement();
 
-    }
-
-    void setCountText()
-    {
-        countText.text = "Count: " + count.ToString();
-
-        if(count >= 12)
-        {
-            winTextObject.SetActive(true);
-        }
-    }
-
-    void FixedUpdate()
-    {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
     }
@@ -60,12 +48,71 @@ public class PlayerController : MonoBehaviour
         {
             //if the game object has a pickup tag, set active status to false
             other.gameObject.SetActive(false);
-            
             //every time "coin" gets picked up add 1 to the count
             count += 1;
-
-            setCountText();
+            SetCountText();
         }
+    }
 
+    private void OnMove(InputValue movementVal)
+    {
+        //getting input value from player
+        // gets vector 2 data from movement value and stores it into the vector2 variable = movementVector
+        Vector2 movementVector = movementVal.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void MouseMovement()
+    {
+        //check if we have any mouse inputs
+        if (Input.GetMouseButton(0))
+        {
+            //if we do then we create a raycast from our screen to the world
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            //if the ray hits something on the ground layer
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundMask))
+            {
+                //we can store the position of the hit point, and calculate the direction from the player to that point
+                Vector3 targetPosition = hit.point;
+                Vector3 direction = (targetPosition - transform.position);
+                direction.y = 0f;
+                direction.Normalize();
+                //and now set our movement vectors to that direction
+                movementX = direction.x;
+                movementY = direction.z;
+            }
+        }
+        //otherwise we set movement to zero
+        else
+        {
+            movementX = 0;
+            movementY = 0;
+            rb.angularVelocity = Vector3.zero;
+            return;
+        }
+    }
+
+    private void GyroscopeMovement()
+    {
+        Debug.Log(Input.gyro.attitude);
+        Vector3 tilt = Input.gyro.gravity;
+        if (tilt.magnitude == 0f) { return; }
+
+        float movementX = tilt.x;
+        float movementY = tilt.y;
+
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 }
